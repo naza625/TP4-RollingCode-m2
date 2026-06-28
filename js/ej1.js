cab592c6b04c4bd20160f767cfcf99b088cd6e5d
naza625 <nazavillafanez@gmail.com>
2026-06-28 17:31:04 -0300

    ej

---
+++ js/ej1.js
@@ -0,0 +1,8 @@
+let meses = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);
+}
